adbab427a873a93ada93ff0db28e9d2b7f594f2f
Fernando Paredes <nano@fdp.io>
2017-05-21 18:00:21 -0700

    Setup a database to access our person dao

---
+++ app/src/main/java/com/pxelated/room/Db.kt
@@ -0,0 +1,13 @@
+package com.pxelated.room
+
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.RoomDatabase
+
+/**
+ * Created by nano on 5/21/17.
+ */
+
+@Database(entities = arrayOf(Person::class), version = 1)
+abstract class Db: RoomDatabase() {
+    abstract fun personDao: PersonDao
+}
