9f093f2e2382fe2dd98a5e5988b21e77297d126c
Fernando Paredes <nano@fdp.io>
2017-05-21 17:51:30 -0700

    Add Person entity

---
+++ app/src/main/java/com/pxelated/room/Person.kt
@@ -0,0 +1,16 @@
+package com.pxelated.room
+
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+/**
+ * Created by nano on 5/21/17.
+ */
+
+@Entity
+data class Person(
+        @PrimaryKey(autoGenerate = true)
+        val uid: Long,
+        val firstName: String = "",
+        val lastName: String = ""
+)
