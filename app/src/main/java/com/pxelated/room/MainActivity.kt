70b9f8001595db60b5ca0c12e203993c8a0fcfc6
Fernando Paredes <nano@fdp.io>
2017-05-21 18:19:33 -0700

    [MainActivity] Allow users to add a person

--- app/src/main/java/com/pxelated/room/MainActivity.kt
+++ app/src/main/java/com/pxelated/room/MainActivity.kt
@@ -2,11 +2,27 @@ package com.pxelated.room
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import io.reactivex.Scheduler
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addPerson("Fernando", "Paredes")
+    }
+
+    fun addPerson(firstName: String, lastName: String) {
+        val person = Person(0, firstName, lastName)
+
+        Single
+            .fromCallable { App.database?.personDao()?.add(person) }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe()
     }
 }
